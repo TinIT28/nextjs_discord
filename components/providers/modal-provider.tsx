9a60a7e23dab41e2712c43f3eb03837ca3c52433
Tin Nguyen <tinitgithub@gmail.com>
2024-01-14 16:17:32 +0700

    Manage Members

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { Fragment, useEffect, useState } from "react";
-import { InviteModal } from "../modals/invite-modal";
-import { EditServerModal } from "../modals/edit-server-modal";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -21,6 +22,7 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </Fragment>
   );
 };
